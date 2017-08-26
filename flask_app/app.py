f5fd0fb37d4f33cdba21bcab0c6b747d716af580
s4322880 <s4322880@student.uq.edu.au>
2017-08-26 13:00:49 +1000

    flask intergration with html/css/js

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,12 @@
-from flask import import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
+
 
 @app.route('/country')
-def get_country_of_city()
+def get_country_of_city():
+	pass
+
