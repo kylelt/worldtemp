1eb96b5f0b911930aa6ab0014520398302785639
Harsh Pandya <hpand3@gmail.com>
2017-08-26 19:46:47 +1000

    Added byte literal to UTF-8 conversion method for dictionary

--- redis/dataIO.py
+++ redis/dataIO.py
@@ -2,6 +2,10 @@ import redis
 
 POOL = redis.ConnectionPool(host='localhost', port=6379, db=0)
 
+def byteToUTF(dictionary):
+    return {k.decode('utf-8'): v.decode('utf-8') \
+                for k, v in dictionary.items()}
+
 def getCountryStats(countryName):
     redis_db = redis.Redis(connection_pool=POOL)
 
@@ -13,16 +17,15 @@ def getCountryStats(countryName):
         '\' does not exist')
     else:
         # Convert from byte literal to unicode
-        response = {k.decode('utf-8'): v.decode('utf-8') \
-                for k, v in countryData.items()}
+        response = byteToUTF(countryData)
         return response
 
 def getCountrySentiment(countryName):
     country = getCountryStats(countryName)
     avgSentiment = float(country['sentiment']) / int(country['count'])
     return avgSentiment
-    
 
+    
 def setCountryStats(countryName, sentiment):
     redis_db = redis.Redis(connection_pool=POOL)
 
@@ -35,8 +38,7 @@ def setCountryStats(countryName, sentiment):
         newCountryData['count'] = 1
     else:
         # Convert from byte literal to unicode
-        newCountryData = {k.decode('utf-8'): v.decode('utf-8') \
-                for k, v in countryData.items()}
+        newCountryData = byteToUTF(countryData)
 
         # Increment the tweet count
         newCountryData['count'] = int(newCountryData['count']) + 1
@@ -50,5 +52,5 @@ def setCountryStats(countryName, sentiment):
     redis_db.hmset(countryName, newCountryData);
 
 # setCountryStats("United States", -0.6)
-# print(getCountryStats("Unisted States"))
-print(getCountrySentiment("United States"))
+# print(getCountryStats("United States"))
+# print(getCountrySentiment("United States"))
